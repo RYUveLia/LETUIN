73efc3d9ecf69e6c92ebd62c38b0210b12dcd593
RYUveLia <32381910+RYUveLia@users.noreply.github.com>
2021-04-10 18:19:08 +0900

    지나간 수업은 안 봐도 되도록 설정
    지난 수업 보기는 pastclass에 생성

--- interview.py
+++ interview.py
@@ -1,5 +1,11 @@
+import os
+import re
 import requests
+from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
+
+fileDir = os.path.dirname(os.path.realpath(__file__))
+os.chdir(fileDir)
 
 LOGIN_URL = 'https://www.letuin.com/login.php'
 LOGIN_DATA = {
@@ -24,13 +30,22 @@ LOGIN_DATA['passwd'] = passwd
 with requests.session() as session:
     res = session.post(LOGIN_URL, data=LOGIN_DATA)
 
-    flag = 0
+    lecture_num = ""
 
-    while flag == 0:
-        print("스크랩을 원하는 강의번호를 입력해주세요")
-        lecture_num = input()
+    while lecture_num is not "-1":
+        if lecture_num == "":
+            print("스크랩을 원하는 강의번호를 입력해주세요")
+            lecture_num = input()
 
         lec_url = ADMIN_URL + LECTURE_URL + lecture_num + "&pageView=50"
+
+        res = session.get(lec_url)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        num_info = soup.select('table.tbl-table-style > tbody > tr > td')
+
+        # pageView = 전체 기수로 새로고침
+
+        lec_url = ADMIN_URL + LECTURE_URL + lecture_num + "&pageView=" + str(re.findall(r'\d+', num_info[0].text)[0])
 
         res = session.get(lec_url)
         soup = BeautifulSoup(res.text, 'html.parser')
@@ -48,20 +63,40 @@ with requests.session() as session:
         else:
             f = open(filename + ".txt", "w")
 
-
-        print("스크랩을 원하는 기수를 입력해주세요(전체: -1)")
-        gisu = int(input())
+        # num_info[i % 7]
+        # 0: 기수
+        # 1: 모집기간
+        # 2: 수강기간
+        # 3: 강사명
+        # 4: 마감여부
+        # 5: 수강생
+        # 6: 비고 
 
         for i in reversed(range(0, len(lec_info), 3)):
-            find_gisu = int(num_info[i // 3 * 7].text)
+            newi = i // 3 * 7
+            # find_gisu = 1기 2기 3기
+            find_gisu = int(re.findall(r'\d+', num_info[newi].text)[0])
 
-            if gisu == -1 or find_gisu == gisu:
+            # class_size = 정원
+            class_size = int(re.findall(r'\d+', num_info[newi + 5].text)[1])
+
+            # 수강 종료날짜가 안지났으면
+            #   0   1  2     3   4  5
+            # YYYY MM DD - YYYY MM DD
+            last_class_array = re.findall(r'\d+', num_info[newi + 2].text)[3:]
+            last_class = last_class_array[0] + "." + last_class_array[1] + "." + last_class_array[2]
+
+            yesterday = datetime.today() - timedelta(1)
+            yesterday = yesterday.strftime("%Y.%m.%d")    # 2021.03.26 형태의 시간 출력
+
+            if yesterday < last_class:
                 line = str(find_gisu) + "기\n"
-                #print(num_info[i * 7 // 2].text)
                 #print(line)
                 f.write(line)
 
-                info_url = ADMIN_URL + lec_info[i].get('href') + "&pageView=50"
+                # https://www.letuin.com/admin/lecture.php?action=LectureDateRegister&lec_type=&lec_no=강의번호&no=3104[기수번호]&w=update
+
+                info_url = ADMIN_URL + lec_info[i].get('href') + "&pageView=" + str(class_size)
 
                 info_res = session.get(info_url)
                 info_soup = BeautifulSoup(info_res.text, 'html.parser')
@@ -91,16 +126,20 @@ with requests.session() as session:
                     f.write(line)
                     #print(line)
 
+                # 빈 자리 엔터치기
+                for j in range(len(std)//2, class_size):
+                    num = num + 1
+                    line = "\t" + str(num) + "\n"
+                    f.write(line)
+
                 f.write("\n")
             
 
         f.close()
 
         print("파일이 생성되었습니다. 종료하시겠습니까?(Y/N)")
-        flag = input()
+        lecture_num = input()
 
-        if flag == 'Y':
-            flag = 1
-        elif flag == 'N':
-            flag = 0
+        if lecture_num == 'Y':
+            lecture_num = "-1"
 
